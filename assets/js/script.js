79efca9ad4f83e4102c1160a76b4c011c9dc4da7
ylf456 <145236890+ylf456@users.noreply.github.com>
2023-10-29 23:20:35 -0700

    fixing in the timing interval

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ var countdownNumber = document.getElementById("time-remaining");
 var quizStartbutton = document.getElementById("start-button");
 var quizDataNumber = document.getElementById("quiz-content");
 var lastQuestionCorrectorNot = document.getElementById("check-last-question");
-var answerListContainer = document.querySelectorAll("li")
+var answerListContainer = document.querySelector("li")
+var index = 0;
 /*
 var question1optionsArray = []
 var question2optionsArray = []
@@ -11,65 +12,73 @@ var question4optionsArray = []
 var question5optionsArray = []
 */
 
-var trueAnswersArray = ["script","The body section","function myFunction()","myFunction()","//This is a comment"];
+var trueAnswersArray = ["script", "The body section", "function myFunction()", "myFunction()", "//This is a comment"];
 
-
-var timeleft = 100;
+var timeleft = 10;
 function countdown() {
     // Sets interval in variable
     var timerInterval = setInterval(function () {
         timeleft = timeleft - 1;
         countdownNumber.textContent = "Time Remaining: " + timeleft;
 
-        if (timeleft === -1) {
+        if (timeleft === 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
+            
             // Calls function that shows the Settlement interface
+           
+            return index = 5;  
         }
     }, 1000);
 }
-
-
+console.log(index)
+console.log(quizDataNumber.children[5])
 quizStartbutton.addEventListener("click", function (event) {
     event.preventDefault();
     countdown();
     iterateOverQuizs();
-  //  quizDataNumber.children[0].setAttribute('class', 'hidden-quiz')
-  //  quizDataNumber.children[1].setAttribute('class', 'revealed-quiz')
 });
 
 //var testingTheAnswer = function () {}
 
-var iterateOverQuizs = function () {  
-   var index = 0;
+var iterateOverQuizs = function (index) {
     quizDataNumber.children[index].setAttribute('class', 'hidden-quiz');
     quizDataNumber.children[index + 1].setAttribute('class', 'revealed-quiz');
-   
-  
-
-    if (index <6 ) {
-        answerListContainer.addEventListener("click",function(event){
+    if (index < 6) {
+        answerListContainer.addEventListener("click", function (event) {
             event.preventDefault();
             var element = event.target;
             if (element.matches("li")) {
                 var checkAnswer = element.getAttribute("textcontent")
-                if (trueAnswersArray.include(checkAnswer)) { index = index +1;
-                    lastQuestionCorrectorNot.textContent = "Last Question= correct!"           
-                } else if (!trueAnswersArray.include(checkAnswer)) {index =index +1;
+                if (trueAnswersArray.includes(checkAnswer)) {
+                    index = index + 1;
                     lastQuestionCorrectorNot.textContent = "Last Question= correct!"
+                } else if (!trueAnswersArray.includes(checkAnswer)) {
+                    index = index + 1;
+                    lastQuestionCorrectorNot.textContent = "Last Question= wrong"
                 }
-            }    
-        })   
-    } 
-    if (index === 6) { return settlementScreen();
-     //call a function fot the settlement screen
+            }
+        })
     }
+    else { clearInterval(timerInterval);
+        return;
+        //call a function fot the settlement screen
     }
+}
 
-var settlementScreen = function(){
+var settlementScreen = function () {}
 
+function saveHighScore() {
+    // Save related form data as an object
+    var highscoreRecord = {
+      nameOrinitial :nameOrinitial.value,
+      score: timeleft.value,
+    };
+    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+    localStorage.setItem('highScoreHistory', JSON.stringify(highscoreRecord));
+  }
 
-}    
+function showHighScore(){}
 
 
 /*  randomnize an array
@@ -82,6 +91,6 @@ points.sort(function(){return 0.5 - Math.random()});
 
 //to hide an element in css:   selector {display:none;}
 
-//to interupt an timer,  clearinterval(var);
+//to interupt a timer,  clearinterval(var);
 
-//string.include()
+//string.includes()
